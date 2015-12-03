9a04af267e6c0c32e64507327a8f38991f746f88
Karthik Balakrishnan <karthikb351@gmail.com>
2015-12-04 00:17:26 +0530

    travis build fixed

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ then
   # repo's gh-pages branch. (All previous history on the gh-pages branch
   # will be lost, since we are overwriting it.) We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force  "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
   echo 'Build successful, deployed to gh-pages.'
 else
   echo "Build successful, but not deploying!"
