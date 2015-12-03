579c36d19e2b7ba09a612b4c788e74936e7ce077
Karthik Balakrishnan <karthikb351@gmail.com>
2015-12-04 00:54:11 +0530

    build all sheets

--- build.js
+++ build.js
@@ -1,23 +1,62 @@
 var Tabletop = require('tabletop');
 var jsonfile = require('jsonfile');
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1kJjzLBoUwr1esfcAhKYFjgqKLD_LQoMLiVe69F3oWmg&output=html';
-var tableTop;
+var async = require('async');
 
 
-function showInfo(dataSet, tabletop) {
-  var obj = {
-    columns: tableTop.sheets("Sheet1")['column_names'],
-    rows: tableTop.sheets("Sheet1").toArray()
-  }
-  var file = 'src/api/aid-needed.json';
 
-  jsonfile.writeFileSync(file, obj)
-  console.log('saved');
+var sheets = [
+{
+  name: "Aid Needed",
+  url: 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1kJjzLBoUwr1esfcAhKYFjgqKLD_LQoMLiVe69F3oWmg&output=html',
+  api: 'aid-needed.json',
+  sheetName: "Sheet1",
+},
+{
+  name: "Contacts",
+  url: 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=16igUy2WV8XIHYXTHl8VB76DdZc27i-yBbBct8gCOtIY&output=html',
+  api: 'contacts.json',
+  sheetName: "Sheet1",
+},
+{
+  name: "Shelter",
+  url: 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1RGLsPlq9prGjyLPSkE1J-JJxs7UcI535ifPmpgtKDxo&output=html',
+  api: 'shelter.json',
+  sheetName: "Sheet1",
+},
+{
+  name: "Aid Available",
+  url: 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1eGsS66xxlAO8MCmfyHS6isbpurwKQ1ucv6DaD31v3F8&output=html',
+  api: 'aid-available.json',
+  sheetName: "Sheet1",
 }
+];
+
+async.forEach(sheets, function (item, callback){
+
+  function writeData(dataSet, tabletop) {
+    var obj = {
+      columns: tabletop.sheets(item.sheetName)['column_names'],
+      rows: tabletop.sheets(item.sheetName).toArray()
+    }
+    var file = 'src/api/'+item.api;
+
+    jsonfile.writeFileSync(file, obj)
+    console.log('Saved: '+item.name);
+    callback();
+  }
+  Tabletop.init( { key: item.url,
+    callback: writeData,
+    wanted: [item.sheetName],
+    simpleSheet: false } )
+
+  }, function(err) {
+    console.log('Done');
+  });  
 
 
-tableTop = Tabletop.init( { key: public_spreadsheet_url,
-  callback: showInfo,
-  wanted: ["Sheet1"],
-  simpleSheet: false } )
+
+
+
+
+
 
